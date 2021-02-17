29d152bec9549a3d8be723e33560016569c0720a
Victor <sirotilov@gmail.com>
2021-02-17 23:44:31 +0200

    fix task-08

--- task-08.js
+++ task-08.js
@@ -41,6 +41,7 @@ function destroyBoxes() {
   boxesRef.innerHTML = "";
 }
 function randomColor() {
-  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+  let whiteColor = 16777215;// 256*256*256 = 16777216
+  return "#" + Math.floor(Math.random() * whiteColor).toString(16);
 }
 
